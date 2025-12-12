6f939d62c8c29d0138f3ad87db3bf9703d5639b3
rachaelowo <Rachaelowolabi89@gmail.com>
2025-12-12 14:38:54 -0500

    Update Mongoose connection to use environment variable

--- server.js
+++ server.js
@@ -11,12 +11,9 @@ const app = express();
 
 require('./config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('MongoDB connection error:', err));
 
 
 app.set('view engine', 'ejs');
