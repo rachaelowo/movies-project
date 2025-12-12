b6dd240c436c9809efad5472d9c632c6150c20aa
rachaelowo <Rachaelowolabi89@gmail.com>
2025-12-12 14:30:00 -0500

    Update mongoose connection URI for Heroku

--- server.js
+++ server.js
@@ -11,9 +11,13 @@ const app = express();
 
 require('./config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
